5874a0805143d94d446fe2f4d6603d4a5cb21ea9
Sean <sean@seanhay.com>
2022-04-17 00:49:22 +0900

    CI final test

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import "./style.css"
 const app = document.querySelector<HTMLDivElement>("#app")!
 
 app.innerHTML = `
-  <h1 class="text-red-500 text-xl">Hello Vite!</h1>
+  <h1 class="text-xl text-teal-500">Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
